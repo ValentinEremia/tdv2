a8335f891b1ec39c83970448d033429fbeb39b8e
ValentinEremia <98782476+ValentinEremia@users.noreply.github.com>
2022-03-08 17:21:46 +0200

    + Date

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,19 @@
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '-' + mm + '-' + yyyy;
+// document.write(today);
+
+document.getElementById("showDate").innerHTML = today;
+
+// var dateTime= new Date();
+//   document.getElementById("showDate").innerHTML = dateTime;
+
+
+
 var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
   todo: [],
   completed: []
